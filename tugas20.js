f1f29a2b3cedd9d73eff596b5064f574f1be0fb4
AirPutih19 <kharismadwisj@gmail.com>
2023-05-17 14:59:52 +0700

    Tugas 20 JS
    Tugas 20 JS untuk niomic

---
+++ tugas20.js
@@ -0,0 +1,8 @@
+function cari(){
+    var isi ="There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.";
+    var nul = null;
+    console.log(isi.match("are",));
+    console.log(nul);
+    console.log(isi.match("words"));
+}
+cari()
